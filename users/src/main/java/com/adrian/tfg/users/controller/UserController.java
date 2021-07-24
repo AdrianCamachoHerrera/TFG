3e08a9581a96495bd871fd4b06c741620673207e
Adrian Camacho <joseadrianch98@gmail.com>
2021-07-24 20:51:04 +0200

    users-ms fix

--- users/src/main/java/com/adrian/tfg/users/controller/UserController.java
+++ users/src/main/java/com/adrian/tfg/users/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,5 +42,4 @@ public class UserController {
 		}	
 	}
 	
-
 }
